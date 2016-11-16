8f7daeb92f22283360b300ccc9ce765dadafee05
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2016-11-16 05:38:13 +0000

    upd

--- lib/prophecy/generators/book/helpers/tex2md.sh
+++ lib/prophecy/generators/book/helpers/tex2md.sh
@@ -7,9 +7,14 @@ cat "$SRC" |\
 sed -e 's/\\label[{][^}]\+[}]//g; s/\\pageref[{][^}]\+[}]/FIXME:pageref/g' |\
 sed 's/\\verseref[{]\([^}]\+\)[}]/\1.\\\\/g' |\
 sed 's/\\thai[{]\([^}]\+\)[}]/\\emph{\1}/g' |\
+sed 's/\\mbox[{]\([^}]\+\)[}]/\1/g' |\
 sed 's/\\speaker[{]\([^}]\+\)[}]/\\emph{\1}/g' |\
 sed 's/\\LaTeX/LaTeX/g' |\
 sed 's/\(\w\)"-\(\w\)/\1-\2/g' |\
+sed 's/\\tocChapterNote{\([^}]\+\)}/\n\\emph{\1}/g' |\
+sed 's/\\QA{\([^}]\+\)}/\\textbf{\1}/g' |\
+sed 's/\\begin{packeditemize}//g' |\
+sed 's/\\end{packeditemize}//g' |\
 pandoc --smart --normalize --from=latex --to=markdown |\
 sed 's/\([^\\]\)\\$/\1\\\\/' > "$DEST"
 
